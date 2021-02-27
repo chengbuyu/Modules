1566e6c8e8714e16ac1b53742174860cefa38c20
chengbuyu <51528684+chengbuyu@users.noreply.github.com>
2021-02-28 02:27:19 +0800

    Update WeChatAds.js

--- Surge/WeChat/WeChatAds.js
+++ Surge/WeChat/WeChatAds.js
@@ -1,8 +1,6 @@
 /*
 Remove the WeChat public account bottom ad
-by Choler
-QX:
-^https?:\/\/mp\.weixin\.qq\.com\/mp\/getappmsgad url script-response-body https://raw.githubusercontent.com/NobyDa/Script/master/QuantumultX/File/Wechat.js
+by NobyDa
 
 Surge4：
 http-response ^https?:\/\/mp\.weixin\.qq\.com\/mp\/getappmsgad requires-body=1,max-size=0,script-path=https://raw.githubusercontent.com/NobyDa/Script/master/QuantumultX/File/Wechat.js
